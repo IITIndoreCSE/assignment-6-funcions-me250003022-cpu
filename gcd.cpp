d2e23ad0e6697a5f9fdb10878b7a2325652975aa
me250003022-cpu <me250003022@iiti.ac.in>
2025-09-04 09:59:31 +0530

    gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 using namespace std;
 
-// Recursive function to find GCD of two numbers
+// Recursive function to calculate GCD
 int gcd(int a, int b) {
-    // TODO: complete the function as per instructions
+    // Base case: when b is 0, return a
+    if (b == 0) {
+        return a;
+    }
     
+    // Recursive case: GCD of b and a % b
+    return gcd(b, a % b);
 }
 
 int main() {
-    int num1, num2;
-    cin >> num1 >> num2;
+    int a, b;
+    
+    // Taking input from the user
+    cout << "Enter two integers: ";
+    cin >> a >> b;
 
-    cout << gcd(num1, num2) << "\n";
-
+    // Output the GCD
+    cout << gcd(a, b) << endl;
+    
     return 0;
 }
-
